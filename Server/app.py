25f10a743dd96176c2a5eb0bf4ac5212f079f4a8
nirglus <nirgluskin@gmail.com>
2024-06-14 08:43:30 +0300

    add cors

--- Server/app.py
+++ Server/app.py
@@ -1,7 +1,9 @@
 from flask import Flask
+from flask_cors import CORS
 from routes.password import password_bp 
 
 app = Flask(__name__)
+CORS(app)
 
 app.register_blueprint(password_bp, url_prefix='/password')
 
