193ecaabbfccfc6c88a06649359d5668d8f8bfde
nirglus <nirgluskin@gmail.com>
2024-06-14 15:31:06 +0300

    add react icons, add copyToClipboard

--- Client/src/components/PasswordGenerator.tsx
+++ Client/src/components/PasswordGenerator.tsx
@@ -1,67 +1,96 @@
-import { useState } from 'react';
-import axios from 'axios';
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { FaCopy } from "react-icons/fa";
+import { IoMdCheckboxOutline } from "react-icons/io";
 
 const PasswordGenerator: React.FC = () => {
-    const [length, setLength] = useState<number>(12);
-    const [useSpecialChars, setUseSpecialChars] = useState<boolean>(false);
-    const [password, setPassword] = useState<string>('');
+  const [length, setLength] = useState<number>(12);
+  const [useSpecialChars, setUseSpecialChars] = useState<boolean>(false);
+  const [password, setPassword] = useState<string>("");
+  const [copySuccess, setCopySuccess] = useState<boolean>(false);
 
-    const generatePassword = async () => {
-        try {
-            const response = await axios.get<{ password: string }>(`${import.meta.env.VITE_API_URL}/password`, {
-                params: {
-                    length: length,
-                    use_special_chars: useSpecialChars
-                }
-            });
-            setPassword(response.data.password);
-        } catch (error) {
-            console.error('Error generating password:', error);
+  const generatePassword = async () => {
+    try {
+      const response = await axios.get<{ password: string }>(
+        `${import.meta.env.VITE_API_URL}/password`,
+        {
+          params: {
+            length: length,
+            use_special_chars: useSpecialChars,
+          },
         }
-    };
-
-    return (
-        <div className="flex flex-col mx-auto min-h-80 p-6 bg-opacity-65 bg-slate-950 shadow-md rounded-lg">
-          <h1 className="text-3xl font-bold text-green-300 mb-4">Password Generator</h1>
-          <div className="mb-4">
-            <label className="block">
-              <span className="text-gray-200">Length: {length}</span>
-              <input
-                type="range"
-                min="1"
-                max="50"
-                value={length}
-                onChange={(e) => setLength(+e.target.value)}
-                className="w-full mt-2"
-              />
-            </label>
-          </div>
-          <div className="mb-4">
-            <label className="flex items-center">
-              <input
-                type="checkbox"
-                checked={useSpecialChars}
-                onChange={(e) => setUseSpecialChars(e.target.checked)}
-                className="mr-2"
-              />
-              <span className="text-gray-200">Use Special Characters</span>
-            </label>
-          </div>
-          <button
-            onClick={generatePassword}
-            className="bg-green-700 hover:bg-green-500 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-          >
-            Generate Password
-          </button>
-          {password && (
-            <div className="mt-4">
-              <h2 className="text-xl font-semibold text-green-300 mb-2">Generated Password:</h2>
-              <p className="text-gray-200">{password}</p>
-            </div>
-          )}
-        </div>
       );
-      
+      setPassword(response.data.password);
+    } catch (error) {
+      console.error("Error generating password:", error);
+    }
+  };
+
+  const copyToClipboard = () => {
+    navigator.clipboard
+      .writeText(password)
+      .then(() => setCopySuccess(true))
+      .catch((err) => console.error("Failed to copy:", err));
+  };
+
+  useEffect(()=>{
+    setCopySuccess(false);
+  },[length, password])
+
+  return (
+    <div className="flex flex-col mx-auto min-h-80 p-6 bg-opacity-65 bg-slate-950 shadow-md rounded-lg">
+      <h1 className="text-3xl font-bold text-green-300 mb-4">
+        Password Generator
+      </h1>
+      <div className="mb-4">
+        <label className="block">
+          <span className="text-gray-200">Length: {length}</span>
+          <input
+            type="range"
+            min="1"
+            max="50"
+            value={length}
+            onChange={(e) => setLength(+e.target.value)}
+            className="w-full mt-2"
+          />
+        </label>
+      </div>
+      <div className="mb-4">
+        <label className="flex items-center">
+          <input
+            type="checkbox"
+            checked={useSpecialChars}
+            onChange={(e) => setUseSpecialChars(e.target.checked)}
+            className="mr-2"
+          />
+          <span className="text-gray-200">Use Special Characters</span>
+        </label>
+      </div>
+      <button
+        onClick={generatePassword}
+        className="bg-green-700 hover:bg-green-500 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+      >
+        Generate Password
+      </button>
+      {password && (
+        <div className="mt-4">
+          <h2 className="text-xl font-semibold text-green-300 mb-2">
+            Generated Password:
+          </h2>
+          <div className="flex justify-between w-full gap-3">
+            <p className="text-gray-200">{password}</p>
+            <button
+                onClick={copyToClipboard}
+                className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            >
+                <FaCopy />
+            </button>
+          </div>
+          {copySuccess && <span className="text-green-500 ml-2 flex items-center gap-1"><IoMdCheckboxOutline /> Copied!</span>}
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default PasswordGenerator;
